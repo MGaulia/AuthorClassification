3b0f92977a6b9e8913c0366e20179b16ee6c0cf3
Matas Gaulia <39590298+MGaulia@users.noreply.github.com>
2022-01-11 01:19:05 +0200

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,228 @@
+from transformers import RobertaPreTrainedModel, RobertaModel
+from transformers.modeling_outputs import SequenceClassifierOutput
+import torch.nn as nn
+from torch.nn import CrossEntropyLoss
+
+
+class RobertaForSequenceClassification2(RobertaPreTrainedModel):
+    def __init__(self, config, **kwargs):
+        super().__init__(transformers.PretrainedConfig())
+        self.num_labels = kwargs.get("task_labels_map", {})
+        self.config = config
+
+        self.roberta = RobertaModel(config)
+        classifier_dropout = (
+            config.classifier_dropout
+            if config.classifier_dropout is not None
+            else config.hidden_dropout_prob
+        )
+        self.dropout = nn.Dropout(classifier_dropout)
+
+    
+        self.classifier1 = nn.Linear(
+            config.hidden_size, list(self.num_labels.values())[0]
+        )
+        self.classifier2 = nn.Linear(
+            config.hidden_size, list(self.num_labels.values())[1]
+        )
+
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+        task_name=None
+    ):
+
+        return_dict = (
+            return_dict if return_dict is not None else self.config.use_return_dict
+        )
+
+        outputs = self.roberta(
+            input_ids = input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        
+        
+        logits = None
+        if task_name == list(self.num_labels.keys())[0]:
+            logits = self.classifier1(pooled_output)
+        elif task_name == list(self.num_labels.keys())[1]:
+            logits = self.classifier2(pooled_output)
+
+        loss = None
+        if labels is not None:
+            loss_fct = CrossEntropyLoss()
+            loss = loss_fct(
+                logits.view(-1, self.num_labels[task_name]), labels.view(-1)
+            )
+
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+import dataclasses
+from torch.utils.data.dataloader import DataLoader
+from transformers.data.data_collator import DataCollator, InputDataClass
+from torch.utils.data.distributed import DistributedSampler
+from torch.utils.data.sampler import RandomSampler
+from typing import List, Union, Dict
+import numpy as np
+import torch
+import transformers
+
+class StrIgnoreDevice(str):
+    """
+    This is a hack. The Trainer is going call .to(device) on every input
+    value, but we need to pass in an additional `task_name` string.
+    This prevents it from throwing an error
+    """
+
+    def to(self, device):
+        return self
+
+
+class DataLoaderWithTaskname:
+    """
+    Wrapper around a DataLoader to also yield a task name
+    """
+
+    def __init__(self, task_name, data_loader):
+        self.task_name = task_name
+        self.data_loader = data_loader
+
+        self.batch_size = data_loader.batch_size
+        self.dataset = data_loader.dataset
+
+    def __len__(self):
+        return len(self.data_loader)
+
+    def __iter__(self):
+        for batch in self.data_loader:
+            batch["task_name"] = StrIgnoreDevice(self.task_name)
+            yield batch
+
+
+class MultitaskDataloader:
+    """
+    Data loader that combines and samples from multiple single-task
+    data loaders.
+    """
+
+    def __init__(self, dataloader_dict):
+        self.dataloader_dict = dataloader_dict
+        self.num_batches_dict = {
+            task_name: len(dataloader)
+            for task_name, dataloader in self.dataloader_dict.items()
+        }
+        self.task_name_list = list(self.dataloader_dict)
+        self.dataset = [None] * sum(
+            len(dataloader.dataset) for dataloader in self.dataloader_dict.values()
+        )
+
+    def __len__(self):
+        return sum(self.num_batches_dict.values())
+
+    def __iter__(self):
+        """
+        For each batch, sample a task, and yield a batch from the respective
+        task Dataloader.
+        We use size-proportional sampling, but you could easily modify this
+        to sample from some-other distribution.
+        """
+        task_choice_list = []
+        for i, task_name in enumerate(self.task_name_list):
+            task_choice_list += [i] * self.num_batches_dict[task_name]
+        task_choice_list = np.array(task_choice_list)
+        np.random.shuffle(task_choice_list)
+        dataloader_iter_dict = {
+            task_name: iter(dataloader)
+            for task_name, dataloader in self.dataloader_dict.items()
+        }
+        for task_choice in task_choice_list:
+            task_name = self.task_name_list[task_choice]
+            yield next(dataloader_iter_dict[task_name])
+
+
+class MultitaskTrainer(transformers.Trainer):
+    def get_single_train_dataloader(self, task_name, train_dataset):
+        """
+        Create a single-task data loader that also yields task names
+        """
+        if self.train_dataset is None:
+            raise ValueError("Trainer: training requires a train_dataset.")
+
+        train_sampler = (
+            RandomSampler(train_dataset)
+            if self.args.local_rank == -1
+            else DistributedSampler(train_dataset)
+        )
+
+        data_loader = DataLoaderWithTaskname(
+            task_name=task_name,
+            data_loader=DataLoader(
+                train_dataset,
+                batch_size=self.args.train_batch_size,
+                sampler=train_sampler,
+                collate_fn=self.data_collator,
+            ),
+        )
+        return data_loader
+
+    def get_train_dataloader(self):
+        """
+        Returns a MultitaskDataloader, which is not actually a Dataloader
+        but an iterable that returns a generator that samples from each
+        task Dataloader
+        """
+        return MultitaskDataloader(
+            {
+                task_name: self.get_single_train_dataloader(task_name, task_dataset)
+                for task_name, task_dataset in self.train_dataset.items()
+            }
+        )
+    
+    
+    def get_eval_dataloader(self,eval_dataset=None):
+        """
+        Returns a MultitaskDataloader, which is not actually a Dataloader
+        but an iterable that returns a generator that samples from each
+        task Dataloader
+        """
+        if eval_dataset is None:
+            eval_dataset = self.eval_dataset
+        dataloader =  MultitaskDataloader(
+            {
+                task_name: self.get_single_train_dataloader(task_name, task_dataset)
+                for task_name, task_dataset in eval_dataset.items()
+            }
+        )
+        dataloader.batch_size = self.args.eval_batch_size
+        return dataloader
